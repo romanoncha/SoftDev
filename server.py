c8c0ef7fabb717e3cbee9d8cfd3bca7d0f8aa997
romanoncha <romanoncha@mail.ru>
2013-05-31 00:19:40 +0300

    Обновлен код сервера. Удален Server_Threading

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ from socket import socket, gethostbyname, AF_INET, SOCK_STREAM
 import errno
 from socket import error
 import sys
-from Client_Threading import ClientThreading
 
 # GLOBAL
 busyportlist = []
@@ -15,14 +14,15 @@ max_client = 20
 
 
 ConnClient = []
+ClientsLogins = []
 
 class Server(object):
 
     def __init__(self,port):
         '''Base constructor'''
         self.port = port
-        self.sock = socket(AF_INET, SOCK_STREAM)
-        self.count = 0 
+        self.sock = socket.socket(AF_INET, SOCK_STREAM)
+        self.count = 0
 
     def Start(self):
         '''Start Server'''
@@ -34,19 +34,19 @@ class Server(object):
             sys.exit("Sorry, error , can't create socket")
         print "Start OK, port = ", self.port
         self.sock.listen(max_client)
-        
+
         while True:
             self.conn, self.adrr = self.sock.accept()
-            ConnClient.append(ClientThreading(self.conn,self.adrr,ConnClient))
+            ConnClient.append(ClientThreading(self.conn,self.adrr))
             ConnClient[self.count].start()
             self.count += 1
 
-            
+
     def Close(self):
         '''Finish work'''
-        for conn in self.ConnClient:
+        for conn in ConnClient:
             conn.Close()
-        sock.close()
+        self.sock.close()
 
     def FindFreePort(self):
         '''Locks for free port'''
@@ -56,3 +56,42 @@ class Server(object):
                 print "Free port ",i
                 self.port = i
                 break
+
+import socket
+import threading
+from Server import *
+
+#Client_Threading CLASS
+class ClientThreading(threading.Thread):
+    def __init__(self, clientSock, addr):
+
+        self.clientSock = clientSock
+        self.addr = addr
+        threading.Thread.__init__(self)
+    def run (self):
+        self.Autorization()
+        while True:
+            self.data = self.clientSock.recv(1024)
+            for client in ConnClient:
+                if client == self:
+                    continue
+                else:
+                    mess = self.login + ": " + self.data
+                    client.clientSock.send(mess)
+
+    def Close(self):
+        self.clientSock.close()
+    def Autorization(self):
+        self.clientSock.send("Hello user, first you must login\n")
+        while True:
+            self.login = self.clientSock.recv(1024)
+            if self.login in ClientsLogins:
+                self.clientSock.send("Sorry, this login is already used, try again\n")
+                continue
+            elif len(ClientsLogins) >= max_client:
+                self.clientSock.send("Sorry, server is overload. Try later\n")
+                continue
+
+            self.clientSock.send("Welcome to our chat "+self.login+"\n")
+            ClientsLogins.append(self.login)
+            break
