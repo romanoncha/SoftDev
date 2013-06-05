35f60a1e1639fe6d2f0fe2a04c4ec9b9753ba8ff
justdude <dude.just.dude@gmail.com>
2013-06-05 22:13:52 +0400

    клиент
    выливаюсь

--- client.py
+++ client.py
@@ -1,18 +1,41 @@
+'''
+Client classes
+@author: ivan
+'''
+import socket
+from threading import Thread
 
-#-*- coding:utf-8 -*-
-from socket import *
-
-class Client(object):
+class ClientInfo:
     def __init__(self):
-        self.sock = socket(AF_INET, SOCK_STREAM)
-    def connect(self, host, port):
-        self.sock.connect((host, port))
-    def send(self,mess):
-        self.sock.send(mess)
-        if mess == "exit_":
-            self.sock.close()
-    def receive(self):
+        self.userName=""
+        self.passWord=""
+
+
+class Client:    
+    def __init__(self):
+        'Client init'
+        self.Socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.data=""
+        self.messageText=""
+       
+    def Connect(self,ip,port):
+        'connect to ip and port, where ip string and port integer'
+        self.Socket.connect((ip,port))
+        print "Connect"
+        
+    def Disconnect(self):
+        'connect to ip and port'
+        self.Socket.close()
+        print "Disconnect"
+        
+    def Send(self,message):
+        'send message to server'
+        self.Socket.send(message)
+        print "Send"
+      
+    def StartReceive(self): 
+        print "StartReceive "
         while True:
-            result = self.sock.recv(1024)
-            print result
-            
+            self.data=self.Socket.recv(1024)
+            if self.data:
+                print "Server Message: "+self.data
